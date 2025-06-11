d33807b32b643a0a6ae9b0e3b059e798ce5cfc4c
Cohen3126430 <3126430@gmail.com>
2025-06-11 16:29:36 +0300

    final project works! angular-material, filter, design, etc...

--- src/app/clients/client-list/clients.component.ts
+++ src/app/clients/client-list/clients.component.ts
@@ -17,8 +17,8 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 export class ClientsComponent implements OnInit {
   
   colDefs: ColDef[] = [
-    { field: "firstName" },
-    { field: "lastName" },
+    { field: "firstName", filter: true},
+    { field: "lastName", filter: true},
     { 
       headerName: "Details", 
       cellRenderer: (params: ICellRendererParams) => {
--- src/app/lessons/lesson-details/lesson-details.component.ts
+++ src/app/lessons/lesson-details/lesson-details.component.ts
@@ -1,26 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Lesson } from '../../Models/lesson.model';
-import { NgIf } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common'; 
+import { NgClass, NgStyle } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { LessonService } from '../lesson-service.service';
 
 @Component({
   selector: 'app-lesson-details',
   standalone: true,
-  imports: [
-    NgIf,
-  ],
+  imports: [MatCardModule, CommonModule, NgClass, NgStyle],
   templateUrl: './lesson-details.component.html',
-  styleUrl: './lesson-details.component.css'
+  styleUrls: ['./lesson-details.component.css'],
 })
-export class LessonDetailsComponent  implements OnInit {
-  lesson: Lesson | null = null;
+export class LessonDetailsComponent implements OnInit {
+  @Input() lesson: Lesson | null = null;
 
   constructor(private route: ActivatedRoute, private lessonService: LessonService) {}
 
   ngOnInit() {
-    this.lesson = this.lessonService.getSelectedLesson(); // קבל את השיעור מהשירות
+    this.lesson = this.lessonService.getSelectedLesson();
   }
-  // @Input() lesson: Lesson | null = null;
 
+  hasLessonStarted(lesson: any): boolean {
+    const now = new Date();
+    return lesson.startDate < now;
+  }
 }
